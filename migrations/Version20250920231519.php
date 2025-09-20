3b535e7449e1aa76ba1cf1cbf7e26a38cd69809a
Ma'ruf Sheraliyev <mfsv1011@mail.ru>
2025-09-21 04:15:43 +0500

    add company

--- migrations/Version20250920231519.php
+++ migrations/Version20250920231519.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250920231316 extends AbstractMigration
+final class Version20250920231519 extends AbstractMigration
 {
     public function getDescription(): string
     {
